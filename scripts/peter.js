490d462e781eec25fb38565abee25f0119afa102
PeterHCM <90168237+PeterHCM@users.noreply.github.com>
2021-10-07 23:55:49 +0700

    Update peter.js

--- scripts/peter.js
+++ scripts/peter.js
@@ -1,6 +1,6 @@
 document.getElementById("btniptvlist").onclick = function () {
 	var x=document.getElementById("valueiptvlist").value;
-	if (x=="Bear@123")
+	if (x=="IPTV@123")
        	{
 		document.getElementById("iptvlist").style.display = 'block';
 	}
